105ada4d5b910c41a90d42431485e98a092927b4
Prateep Kul <1.0@kul.com>
2014-09-29 21:01:58 +0700

    Initial Commit

---
+++ oTo/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  oTo
+//
+//  Created by Kul on 9/29/2557 BE.
+//  Copyright (c) 2557 Kul.com. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
